e527538698e27bcc054316ace3dbaa593061b7e9
JoaWe <33264029+JoaWe@users.noreply.github.com>
2017-11-14 19:23:26 +0200

    deleting combined variables
    Something was wrong in my attempts to combine variables. I am restarting and have therefore deleted the code up to this point with regard to those variables.

--- data/create_learning2014.R
+++ data/create_learning2014.R
@@ -27,27 +27,14 @@ lrn14$Age
 summary(lrn14$Age)
 # The summary provided me with the age-range of the respondents from 17 to 55 years. The average-age of the respondents is 25,58 years. The median-age is 22.
 
-
-# attempting to create new combined variable "deep_learning"
-deep_learning <- c("D03", "D11", "D19", "D27", "D07", "D14", "D22", "D30","D06",  "D15", "D23", "D31")
-#printing and summarize new variable "deep":
-deep_learning
-summary(deep_learning)
-# Not sure if I did that right...
-
-#attempting to create new variable "surface_questions"
-surface_questions <- c("SU02","SU10","SU18","SU26", "SU05","SU13","SU21","SU29","SU08","SU16","SU24","SU32")
-# printing and summarizing of "surface_question"
-surface_questions
-summary(surface_questions)
-
-#attempting to create new variable "strategic_questions"
-strategic_questions <- c("ST01","ST09","ST17","ST25","ST04","ST12","ST20","ST28")
-#printing and summarizing "strategic_questions"
-strategic_questions
-summary(strategic_questions)
-
 #printing and summarizing "Points"
 lrn14$Points
 summary(lrn14$Points)
 # The points reach from 0 to a maximum of 33. The average exam-points are 20,61. The Median-value of the Variable points is 22.00.
+
+#Scaling down "Attitude" to attitude
+lrn14$attitude <- lrn14$Attitude / 10
+lrn14$attitude
+summary(lrn14$attitude)
+#At least that looks like it works...
+
